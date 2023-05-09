08bb260712677f6f59b2263b164161b7c866d5a4
FynnHer <fynnher@ohg-landau.info>
2023-05-09 19:47:11 +0200

    anmeldung

--- app/Http/Controllers/beispiel.php
+++ app/Http/Controllers/beispiel.php
@@ -2,15 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+
 use Illuminate\Http\Request;
 
 class beispiel extends Controller
 {
     //
     public function verarbeiten(Request $request){
+
+        $user = new User();
+
+        $user->lname=$request['lname'];
+        $user->fname=$request['fname'];
+        $user->email=$request['email'];
+        $user->password=$request['password'];
+
         return view('ausgabe', [
             'lname' => $request['lname'],
             'fname' => $request['fname']
         ]);
+
+
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,8 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'fname',
+        'lname',
         'email',
         'password',
     ];
@@ -41,4 +42,6 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $table = 'users';
 }
